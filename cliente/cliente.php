e6d22e1f45bfb7f715bce17139da95c435d74405
pablo antonio gervacio rodriguez <gervaciopabloantonio@gmail.com>
2023-01-21 14:49:47 -0600

    agrendando identificacion  por medio de php
    se creara un login la primera etapa es hacerlo con sessiones primeramente se hara que funcione el login y despues hacer que el login
    -identifique
    -actualize
    -permita recuperacion
    
    primera etapa se realiza autorizacion sin sha o md5 segunda etapa es hasear la informacion y hacer que sea mas segura las sessiones

--- cliente/cliente.php
+++ cliente/cliente.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "../conn/header.php"?>
     <title>Document</title>
 </head>
 <body>
+
+<?php 
+if(isset($_POST))
+?>
+
     <form action="">
             Nombre(s): <input type="text" name="" id=""> <br>
             Apellido Paterno: <input type="text" name="" id=""> <br>
-            apellido Materno: 
+            Apellido Materno: <input type="text" name="" id=""><br>
+            Telefono: <input type="text" name="" id=""> <br>
+            Email: <input type="text" name="" id=""> <br>
+            <input type="submit" name="Guardar" value="Guardar Cliente">
     </form>
 </body>
 </html>
--- login/login.php
+++ login/login.php
@@ -4,11 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "../conn/header.php"?>
     <title>Iniciar Sesion</title>
 </head>
 <body>
- 
 
+<?php 
+if(isset($_POST['Entrar']))
+{
+    $nombre = $_POST['nombre'];
+    $contra = $_POST['contra'];
+    include '../conn/conn.php';
+    $existe = "select nombre from accesocliente where nombre like '$nombre' and password like '$contra'";
+    $consulta = mysqli_query($comprobar, $conn);
+    $dato = mysqli_fetch_array($consulta);
+    $cambia = $dato["nombre"];
+    echo "<hr size == 10 color =fffff width =100% aling = left>";
+        if($dato == ""){
+        echo "los datos estan equivocados";
+        }else{
+        echo "los datos son correctos";
+        }
+    mysqli_close($conn);
+}else{
+?>
+    <form action="" method="post">
+            <i class="bi bi-person"></i>
+            <br>
+            Nombre:<br>
+            <input type="text" name="nombre"><br>
+            <i class="bi bi-key"></i><br>
+            Contraseña:<br>
+            <input type="text" name="contra">
+            <br>
+            <input type="submit" value="Entrar">
+    </form>
+<?php }?>
 </body>
 </html>
 
