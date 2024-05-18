9bc50aa1f2b60c47b45e0868ec40aacd73a41046
pablo antonio gervacio rodriguez <gervaciopabloantonio@gmail.com>
2024-05-18 12:32:25 -0600

    se crearan funciones
    esto es para comprobar si existen los elementos ya en la base de datos todo ello para mas formalidad

--- FuncionesPhp/Funciones.php
+++ FuncionesPhp/Funciones.php
@@ -3,5 +3,8 @@ require_once('php/Cors.php');
 require_once('conn/conn.php');
 $Conectar = conexion();
 
-
+function ExisteProducto(){
+    //esta funcion comprueba si existe o no un producto esto es para evitar duplicidad
+    
+}
 ?>
